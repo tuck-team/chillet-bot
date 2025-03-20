312e8a2f5c7f7c79549dfa2edc7fc6762cc27b0c
lycoris69 <simeon.petrou@epitech.eu>
2025-03-20 14:39:45 +0100

    [ADD] tier.js

---
+++ src/tier.js
@@ -0,0 +1,41 @@
+const { EmbedBuilder } = require('discord.js');
+
+function tier(messageAuthor, userData, replyFunc) {
+	const userPalData = userData[messageAuthor.id];
+
+	// Create embed
+	const embed = new EmbedBuilder()
+		.setColor('#8C1C98')
+		.setDescription(
+			`Current Tier: **${userPalData.tier}**\n` +
+            `Tier 1: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 2: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 3: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 4: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 5: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 6: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 7: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `Tier 8: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+            `!tupgrade to upgrade your tier <:Money:1352019542565720168><:Money:1352019542565720168>`
+		);
+
+	replyFunc({ embeds: [embed] });
+}
+
+function tupgrade(messageAuthor, userData, replyFunc) {
+	const userPalData = userData[messageAuthor.id];
+
+	if (userPalData.gold < 1000) {
+		replyFunc(`You don't have enough gold to upgrade your tier. You need **1000** <:Money:1352019542565720168>`);
+		return;
+	}
+    if (userPalData.tier >= 8) {
+        replyFunc(`You have reached the maximum tier. You can't upgrade anymore.`);
+        return;
+    }
+	userPalData.gold -= 1000;
+	userPalData.tier += 1;
+	replyFunc(`You have upgraded your tier to **${userPalData.tier}**`);
+}
+
+module.exports = { tier, tupgrade };
