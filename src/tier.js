6e723f9aa4f9e03446f124246e1812a0cf9427ea
nemesis-deb <eloi.tisserand@epitech.eu>
2025-03-20 16:16:55 +0100

    [ADD] Updated Tier Embed

--- src/tier.js
+++ src/tier.js
@@ -7,15 +7,17 @@ function tier(messageAuthor, userData, replyFunc) {
 	const embed = new EmbedBuilder()
 		.setColor('#FFD700')
 		.setDescription(
-			`Current Tier: **${userPalData.tier}**\n` +
-            `Tier 1: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 2: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 3: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 4: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 5: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 6: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 7: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
-            `Tier 8: cost : **1000** <:Money:1352019542565720168> Unlocks Expeditions\n\n` +
+			`Current Tier: **${userPalData.tier}**\n\n` +
+			`<:T_itemicon_PalSphere:1352291984953577542> Tier 1: **1000** <:Money:1352019542565720168> Unlocks Expeditions\n` +
+			`<:T_itemicon_PalSphere_Mega:1352292054679552050> Tier 2: **2000** <:Money:1352019542565720168> ???\n` +
+			`<:T_itemicon_PalSphere_Giga:1352292073616707718> Tier 3: **3000** <:Money:1352019542565720168> Unlocks Trading\n` +
+			`<:T_itemicon_PalSphere_Tera:1352292095842586747> Tier 4: **4000** <:Money:1352019542565720168> +1 Expedition Slot\n` +
+			`<:T_itemicon_PalSphere_Master:1352292134136320033> Tier 5: **5000** <:Money:1352019542565720168> -5hr Trading Cooldown\n` +
+			`<:T_itemicon_PalSphere_Legend:1352292167623905300> Tier 6: **6000** <:Money:1352019542565720168> ???\n` +
+			`<:T_itemicon_PalSphere_Robbery:1352292188142305351> Tier 7: **7000** <:Money:1352019542565720168> ???\n` +
+			`<:T_itemicon_PalSphere_Ultimate:1352292204609015858> Tier 8: **8000** <:Money:1352019542565720168> ???\n` +
+			`<:T_itemicon_PalSphere_Exotic:1352292228105764906> Tier 9: **9000** <:Money:1352019542565720168> ???\n` +
+			`\n` +
             `**!tupgrade** to upgrade your tier <:Money:1352019542565720168><:Money:1352019542565720168>`
 		);
 
